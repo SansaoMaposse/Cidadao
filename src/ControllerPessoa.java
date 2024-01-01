3ce098036b2fb8734b1d9dd58b0f2f7836825724
Sansão Maposse <121869087+SansaoMaposse@users.noreply.github.com>
2024-01-01 13:54:24 +0300

    criacao do metodo listarPessoas
    Nesse metodo será responsavel por listar todos os dados insiridos no primeiro metodo.  Usei ArrayList para armazenar todos os dados que serão recebdio...

--- src/ControllerPessoa.java
+++ src/ControllerPessoa.java
@@ -1,16 +1,43 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ControllerPessoa {
 	
-	public static void escreverNoFiche (int id, String nome, String genero, String estadoCivil, int anoNascimento, int anoActual) throws IOException{
+	public static void escreverNoFicheiro (int id, String nome, String genero, String estadoCivil, int anoNascimento, int anoActual) throws IOException{
 		
 		BufferedWriter buffer = new BufferedWriter(new FileWriter("pessoa.txt", true));
 		Pessoa pessoa = new Pessoa(id, nome, genero, estadoCivil, anoNascimento, anoActual);
 		buffer.write(pessoa.toString());
 		buffer.newLine();
 		buffer.close();
+	}
+	
+	public static ArrayList<Pessoa> listarPessoas () throws IOException{
+		
+		BufferedReader buffer = new BufferedReader(new FileReader("pessoa.txt"));
+		ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+		String linha, pessoa[];
+		
+		linha = buffer.readLine();
+		
+		while(linha != null && !linha.isEmpty()) {
+			pessoa = linha.split("-");
+			int id = Integer.parseInt(pessoa[0]);
+			String nome = pessoa [1];
+			String genero = pessoa [2];
+			String estadoCivil = pessoa [3];
+			int anoNascimento = Integer.parseInt(pessoa[4]);
+			int anoActual = Integer.parseInt(pessoa[5]);
+			
+			pessoas.add(new Pessoa(id, nome, genero, estadoCivil, anoNascimento, anoActual));
+			linha = buffer.readLine();
+		}
+		buffer.close();
+		return pessoas;
 	}
 
 }
